353e51b5142002d0108aa617b9a1af051258389e
ayusmadi <abdul-rahman.yusmadi@dxc.com>
2020-01-24 12:20:35 +0800

    Add delete.sh

---
+++ delete.sh
@@ -0,0 +1,8 @@
+# Delete application insights webtest (pingtest) along with its alert
+# Usage: <cmd> <resource_group_name> <name_prefix>
+
+RESOURCE_GROUP_NAME=$1
+NAME=$2
+
+az monitor metrics alert delete -g $RESOURCE_GROUP_NAME -n "${NAME}-pingtest-alert"
+az rest -m DELETE -u "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Insights/webtests/${NAME}-pingtest?api-version=2015-05-01"
