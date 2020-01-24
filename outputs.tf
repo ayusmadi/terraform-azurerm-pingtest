4f2164a4d672253957b99c15d8877f6dc263ae66
ayusmadi <57968900+ayusmadi@users.noreply.github.com>
2020-01-24 09:49:22 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "template_deployment_id" {
-  value = azurerm_template_deployment.main.id
+  value       = azurerm_template_deployment.main.id
+  description = "The Template Deployment ID"
 }
 
 output "template_deployment_outputs" {
-  value = azurerm_template_deployment.main.outputs
+  value       = azurerm_template_deployment.main.outputs
+  description = "A map of supported scalar output types returned from the deployment (currently, Azure Template Deployment outputs of type String, Int and Bool are supported, and are converted to strings - others will be ignored) and can be accessed using .outputs["name"]."
 }
