c625cc1ba6ac9d999de7a061dd25a2815589dbc7
ayusmadi <abdul-rahman.yusmadi@dxc.com>
2020-01-24 15:51:47 +0800

    dev

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
-variable "prefix" {
+variable "prefixes" {
+  type        = list(string)
   description = "Name and also prefix for pingtest and alert"
 }
 
-variable "ping_url" {
+variable "ping_urls" {
+  type        = list(string)
   description = "The URL can be any web page you want to test, but it must be visible from the public internet. The URL can include a query string. So, for example, you can exercise your database a little. If the URL resolves to a redirect, we follow it up to 10 redirects."
 }
 
